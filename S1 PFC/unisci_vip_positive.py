4d6cc7485c20af8274e30483496431c4aac5eac1
Manuel <manufeline@gmail.com>
2025-07-30 21:14:59 +0200

    aggiungi S1 PFC

---
+++ S1 PFC/unisci_vip_positive.py
@@ -0,0 +1,24 @@
+
+
+import pandas as pd
+
+# Carica i due file Excel
+df_alpha7 = pd.read_excel("VIP_positive_alpha7.xlsx")
+df_beta2 = pd.read_excel("VIP_positive_beta2.xlsx")
+
+# Opzionale: rinomina le colonne duplicate aggiungendo un suffisso
+df_alpha7 = df_alpha7.add_suffix("_alpha7")
+df_beta2 = df_beta2.add_suffix("_beta2")
+
+# Rimuovi colonne ridondanti duplicate tra i due (se presenti)
+# Ad esempio, se entrambi hanno 'ImageNumber_alpha7' e 'ImageNumber_beta2' con lo stesso contenuto, puoi tenerne solo una
+
+# Unisci le due tabelle orizzontalmente (sulle righe)
+df_unito = pd.concat([df_alpha7, df_beta2], axis=1)
+
+# Esporta il risultato (opzionale)
+df_unito.to_excel("VIP_positive_unito.xlsx", index=False)
+
+# Visualizza le prime righe per controllo
+print(df_unito.head())
+
